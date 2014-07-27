d9dc3c477a092b35fb355c5d4a4270bb3595a969
Hari Donthi <hari.donthi@gmail.com>
2014-07-26 22:07:45 -0400

    First push

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-tidyData
-========
+---
+title: "README.md"
+output: html_document
+---
 
-Tidy Dataset corresponding to UCI HAR data - Course Project 
+**This document explains the steps that were taken from the raw data to create the Tidy dataset.**
+
+1. Merges the training and the test sets to create one data set.
+    * cbind with X_ and y_
+    * keep only mean() and std()
+    * rbind X_test.txt and X_train.txt
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+    * features.txt %in% mean(), std()
+3. Uses descriptive activity names to name the activities in the data set
+    * cbind with y_test
+    * merge y_test in activity_labels
+4. Appropriately labels the data set with descriptive variable names. 
+    * create space separated camel case columns and add to first row
+5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+    * extract only variables with %in% mean() in variable names
---
+++ run_analysis.R
@@ -0,0 +1,45 @@
+
+##subset columns - list of features to get
+features <- read.csv("features.txt", sep=" ", header=FALSE, col.names=c("featureId", "featureName"))
+meanstdFeatures <- subset(features, grepl("mean|std", featureName, ignore.case=FALSE))
+
+# read X_test
+xTest <- read.table("X_test.txt", header=FALSE)
+
+#select columns/features with mean and std in name
+meanstdTestX <- xTest[, meanstdFeatures$featureId]
+
+#create readable column names
+names(meanstdTestX) <- meanstdFeatures$featureName
+
+# read y_test
+yTest <- read.table("y_test.txt", header=FALSE)
+names(yTest) <- "activityId"
+
+testFinal <- cbind(yTest,meanstdTestX)
+
+# Do the same for Train dataset - just as we did for Test
+
+xTrain <- read.table("X_train.txt", header=FALSE)
+
+#select columns/features with mean and std in name
+meanstdTrainX <- xTrain[, meanstdFeatures$featureId]
+
+#create readable column names
+names(meanstdTrainX) <- meanstdFeatures$featureName
+
+# read y_train
+yTrain <- read.table("y_train.txt", header=FALSE)
+names(yTrain) <- "activityId"
+
+## cbind Y_test to  X_test and Y_train to  X_train
+trainFinal <- cbind(yTrain,meanstdTrainX)
+
+# combine test and train dataset
+combinedData <- rbind(testFinal,trainFinal)
+
+## lookup readable names for activityId
+activityLkp <- read.table("activity_labels.txt", col.names=c("activityId","activityName"))
+
+## output final dataset
+tidyFinal <- merge(activityLkp, combinedData)
